60adfaf6d3d8ae2e8d2f8312d285730057eb3d7d
Samm Caagbay <sammcaag@gmail.com>
2026-01-12 15:27:10 +0800

    add: adding a title header and deleting the recipe management, moving the adding of a new dish into the search bar

--- app/components/search-bar.tsx
+++ app/components/search-bar.tsx
@@ -1,5 +1,7 @@
 import React, { type Dispatch, type SetStateAction } from "react";
 import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import { Plus } from "lucide-react";
 
 /* 2. Search Functionality ○
       Users can search for recipes by name or ingredient ○ Implement a search
@@ -15,11 +17,16 @@ export default function SearchBar({
   return (
     <div className="flex flex-col flex-1 gap-4">
       <h4 className="min-w-fit font-semibold text-base">Search for Recipes</h4>
-      <Input
-        placeholder="Search..."
-        value={searchQuery}
-        onChange={(e) => setSearchQuery(e.target.value)}
-      />
+      <div>
+        <Input
+          placeholder="Search..."
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <Button>
+          <Plus /> Add New Dish
+        </Button>
+      </div>
     </div>
   );
 }
--- app/welcome/recipe-box.tsx
+++ app/welcome/recipe-box.tsx
@@ -1,15 +1,20 @@
+import { Utensils } from "lucide-react";
 import { useState } from "react";
 import FavoritesSection from "~/components/favorites-section";
 import RecipeDisplay from "~/components/recipe-display";
-import RecipeManagement from "~/components/recipe-management";
 import SearchBar from "~/components/search-bar";
 
 export function RecipeBox() {
   const [searchQuery, setSearchQuery] = useState("");
   return (
     <main className="flex flex-col pt-16 pb-4 gap-12 max-w-5xl mx-auto">
+      <div className="flex items-center mx-auto gap-4">
+        <div className="p-3 rounded-full bg-primary">
+          <Utensils className="text-background " />
+        </div>
+        <h1 className="text-center font-bold text-4xl">Recipe Box</h1>
+      </div>
       <SearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
-      <RecipeManagement />
       <RecipeDisplay searchQuery={searchQuery} />
       <FavoritesSection />
 
